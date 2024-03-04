412fc5a685c7c127d92167a230bc0a323476e6c2
broccoli <lekker.broccoli@gmail.com>
2024-03-04 14:00:52 +0100

    initial commit

---
+++ src/test/java/com/chrispbacon/chesschat/ChesschatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chrispbacon.chesschat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChesschatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
