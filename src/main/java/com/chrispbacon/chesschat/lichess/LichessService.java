4b6082a3c49bfc31a9a37bcb412a5f4517335647
broccoli <lekker.broccoli@gmail.com>
2024-03-06 12:44:27 +0100

    formatted stats

--- src/main/java/com/chrispbacon/chesschat/lichess/LichessService.java
+++ src/main/java/com/chrispbacon/chesschat/lichess/LichessService.java
@@ -3,6 +3,10 @@ package com.chrispbacon.chesschat.lichess;
 import com.chrispbacon.chesschat.chat.ChatController;
 import com.chrispbacon.chesschat.chat.ChatMessage;
 import com.chrispbacon.chesschat.chat.MessageType;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import org.jetbrains.annotations.Nullable;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
@@ -16,7 +20,7 @@ public class LichessService {
     private final String STATS_PREFIX = "$stats ";
     private final String USER_STATS_URL = "https://lichess.org/api/user/";
 
-    private final String SENDER = "LiChessService";
+    private final String SENDER = "LiChess Bot";
 
     private static final Logger log = LoggerFactory.getLogger(LichessService.class);
 
@@ -42,9 +46,48 @@ public class LichessService {
             return new ChatMessage(errorMessage, SENDER, MessageType.CHAT);
         }
         if (responseEntity.getStatusCode().is2xxSuccessful()) {
-            String responseData = responseEntity.getBody();
+            final String responseData = extractStats(responseEntity);
             return new ChatMessage(responseData, SENDER, MessageType.CHAT);
         }
         return chatMessage;
+    }
+
+    private String extractStats(ResponseEntity<String> responseEntity) {
+        String responseData = responseEntity.getBody();
+        if (responseData != null && !responseData.isEmpty()){
+            try {
+                JsonParser jsonParser = new JsonParser();
+                JsonObject jsonObject = jsonParser.parse(responseData).getAsJsonObject();
+                return formatStats(jsonObject);
+            } catch (Exception e) {
+                log.error("Error while parsing LiChess stats response", e);
+            }
+        }
+        return "Unable to parse LiChess stats response.";
+    }
+
+    private String formatStats(final JsonObject jsonObject) {
+        final String username = jsonObject.get("username").getAsString();
+        final JsonObject perfs = jsonObject.getAsJsonObject("perfs");
+        final String blitzGames = perfs.getAsJsonObject("blitz").get("games").getAsString();
+        final String blitzRating = perfs.getAsJsonObject("blitz").get("rating").getAsString();
+        final String bulletGames= perfs.getAsJsonObject("bullet").get("games").getAsString();
+        final String bulletRating = perfs.getAsJsonObject("bullet").get("rating").getAsString();
+        final String rapidGames = perfs.getAsJsonObject("rapid").get("games").getAsString();
+        final String rapidRating = perfs.getAsJsonObject("rapid").get("rating").getAsString();
+        final String profileUrl = jsonObject.get("url").getAsString();
+        return """
+            LiChess Stats for %s:
+            Blitz
+                Games:  %s
+                Rating: %s
+            Bullet
+                Games:  %s
+                Rating: %s
+            Rapid
+                Games:  %s
+                Rating: %s
+            Profile URL: %s
+            """.formatted(username, blitzGames, blitzRating, bulletGames, bulletRating, rapidGames, rapidRating, profileUrl);
     }
 }
