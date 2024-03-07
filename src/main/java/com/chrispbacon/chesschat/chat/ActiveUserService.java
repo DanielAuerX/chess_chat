1a8c2b394476a31fab9e7e61f0b03e20a0f0624a
broccoli <lekker.broccoli@gmail.com>
2024-03-07 17:33:02 +0100

    added active user panel
    css is pain

---
+++ src/main/java/com/chrispbacon/chesschat/chat/ActiveUserService.java
@@ -0,0 +1,38 @@
+package com.chrispbacon.chesschat.chat;
+
+import com.chrispbacon.chesschat.config.WebSocketConfig;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.stereotype.Component;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Component
+public class ActiveUserService {
+
+    private final Set<String> activeUsers = new HashSet<>();
+    private final SimpMessagingTemplate messagingTemplate;
+
+    public ActiveUserService(SimpMessagingTemplate messagingTemplate) {
+        this.messagingTemplate = messagingTemplate;
+    }
+
+    public Set<String> getActiveUsers() {
+        return activeUsers;
+    }
+
+    public void addUser(final String username) {
+        activeUsers.add(username);
+        broadcastActiveUsers();
+    }
+
+    public void removeUser(final String username) {
+        activeUsers.remove(username);
+        broadcastActiveUsers();
+    }
+
+    private void broadcastActiveUsers() {
+        messagingTemplate.convertAndSend("/topic/userlist", new HashSet<>(activeUsers));
+    }
+
+}
