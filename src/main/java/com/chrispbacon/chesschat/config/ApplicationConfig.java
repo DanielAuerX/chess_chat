d83c8ac93021b3bffabfd02f12f22cc31657e2ed
broccoli <lekker.broccoli@gmail.com>
2024-03-07 17:31:20 +0100

    added time out for web requests

--- src/main/java/com/chrispbacon/chesschat/config/ApplicationConfig.java
+++ src/main/java/com/chrispbacon/chesschat/config/ApplicationConfig.java
@@ -2,12 +2,17 @@ package com.chrispbacon.chesschat.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.SimpleClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
 
 @Configuration
 public class ApplicationConfig {
     @Bean
     public RestTemplate restTemplate() {
-        return new RestTemplate();
+        int timeout = 5000;
+        SimpleClientHttpRequestFactory requestFactory = new SimpleClientHttpRequestFactory();
+        requestFactory.setConnectTimeout(timeout);
+        requestFactory.setReadTimeout(timeout);
+        return new RestTemplate(requestFactory);
     }
 }
