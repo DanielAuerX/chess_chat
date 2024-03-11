e87f87c4e8293a5235f75b05c0d3eaa880be6981
reiss timo <t.reiss@eos-ts.com>
2024-03-11 11:51:55 +0100

    ewej

--- src/main/java/com/chrispbacon/chesschat/auth/PageController.java
+++ src/main/java/com/chrispbacon/chesschat/auth/PageController.java
@@ -1,18 +1,15 @@
 package com.chrispbacon.chesschat.auth;
 
-import com.chrispbacon.chesschat.chat.ChatController;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
 @RestController
-public class TestController {
+public class PageController {
 
-    private static final Logger log = LoggerFactory.getLogger(TestController.class);
+    private static final Logger log = LoggerFactory.getLogger(PageController.class);
 
     @GetMapping("/home")
     public ModelAndView welcome() {
