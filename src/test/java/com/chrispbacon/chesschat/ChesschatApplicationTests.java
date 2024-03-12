ad8c224be16c0f690cf627f96a34ceed63b36e8b
broccoli <lekker.broccoli@gmail.com>
2024-03-12 20:03:52 +0100

    spotlessCheck instead of spotlessApply

--- src/test/java/com/chrispbacon/chesschat/ChesschatApplicationTests.java
+++ src/test/java/com/chrispbacon/chesschat/ChesschatApplicationTests.java
@@ -1,9 +1,8 @@
 package com.chrispbacon.chesschat;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-//@SpringBootTest
+// @SpringBootTest
 class ChesschatApplicationTests {
 
   @Test
