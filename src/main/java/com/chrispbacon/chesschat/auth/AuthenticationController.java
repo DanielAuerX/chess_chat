15225fd01452229a32e04ac45a8f18bdbc692ff7
reiss timo <t.reiss@eos-ts.com>
2024-03-12 09:14:45 +0100

    conflict

--- src/main/java/com/chrispbacon/chesschat/auth/AuthenticationController.java
+++ src/main/java/com/chrispbacon/chesschat/auth/AuthenticationController.java
@@ -4,8 +4,6 @@ import com.chrispbacon.chesschat.config.JwtService;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -23,8 +21,6 @@ import java.io.IOException;
 public class AuthenticationController {
 
 	private final AuthenticationService authenticationService;
-
-	private static final Logger log = LoggerFactory.getLogger(AuthenticationController.class);
 
 	private final JwtService jwtService;
 
---
+++ src/main/java/com/chrispbacon/chesschat/auth/TestController.java
@@ -0,0 +1,37 @@
+package com.chrispbacon.chesschat.auth;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+@RestController
+public class TestController {
+
+    private static final Logger log = LoggerFactory.getLogger(TestController.class);
+
+    @GetMapping("/home")
+    public ModelAndView welcome() {
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("index.html");
+        return modelAndView;
+    }
+
+    @GetMapping("/login")
+    public ModelAndView login() {
+        log.info("test");
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("login.html");
+        log.info(modelAndView.getViewName());
+        return modelAndView;
+    }
+
+    @GetMapping("/register")
+    public ModelAndView register() {
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("register.html");
+        return modelAndView;
+    }
+
+}
